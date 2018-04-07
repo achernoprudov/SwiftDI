3f2f6309c2720069d9e49c64c3a29b5b9d5574ed
Andrey Chernoprudov <achernoprudov@naumen.ru>
2018-04-07 15:22:14 +0500

    injector v1 ready

--- Source/Injector.swift
+++ Source/Injector.swift
@@ -25,8 +25,26 @@ open class Injector {
     
     open func resolve<T>(_ type: T.Type, tag: String = "") -> T {
         let key = InjectorKey(type: type, tag: tag)
-        let provider = dependencies[key]
-        return provider?.provide(by: self) as! T
+        if let provider = dependencies[key] {
+            let dependency = provider.provide(by: self)
+            guard let result = dependency as? T else {
+                let subjectType = Mirror(reflecting: dependency).subjectType
+                preconditionFailure("Provided object has wrong type. Expected '\(type)' but was \(subjectType)")
+            }
+            return result
+        }
+        guard let p = parent else {
+            preconditionFailure("Cant find any parent Injector that could provide '\(type)' with tag='\(tag)'")
+        }
+        return p.resolve(type, tag: tag)
+    }
+    
+    open func resolveSafe<T>(_ type: T.Type, tag: String = "") -> T? {
+        let key = InjectorKey(type: type, tag: tag)
+        if let provider = dependencies[key] {
+            return provider.provide(by: self) as? T
+        }
+        return parent?.resolve(type, tag: tag)
     }
     
    // MARK: - Public

4262e6e85c00b916ec0661e7c72b82cf81ed0e67
Andrey Chernoprudov <dinloq@gmail.com>
2019-06-15 10:33:53 +0500

    App example

---
+++ Samples/Notes/DetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewController.swift
+//  Notes
+//
+//  Created by Andrey Chernoprudov on 15/06/2019.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var detailDescriptionLabel: UILabel!
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+}

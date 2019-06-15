8b8a0be724eefbc26d20f0e03de0056654f21801
Andrey Chernoprudov <dinloq@gmail.com>
2019-06-15 09:52:08 +0500

    parametrized binding up to 13 parameters

--- Sources/SwiftDI/BindingBuilder/BindingBuilder+Parametrized.swift
+++ Sources/SwiftDI/BindingBuilder/BindingBuilder+Parametrized.swift
@@ -32,4 +32,74 @@ public extension BindingBuilder {
             return binding(params)
         }
     }
+
+    func with<P1, P2, P3, P4>(_ binding: @escaping ((P1, P2, P3, P4)) -> T) {
+        injector.bind(builder: self) { i in
+            let params = (i.rp(P1.self), i.rp(P2.self), i.rp(P3.self), i.rp(P4.self))
+            return binding(params)
+        }
+    }
+
+    func with<P1, P2, P3, P4, P5>(_ binding: @escaping ((P1, P2, P3, P4, P5)) -> T) {
+        injector.bind(builder: self) { i in
+            let params = (i.rp(P1.self), i.rp(P2.self), i.rp(P3.self), i.rp(P4.self), i.rp(P5.self))
+            return binding(params)
+        }
+    }
+
+    func with<P1, P2, P3, P4, P5, P6>(_ binding: @escaping ((P1, P2, P3, P4, P5, P6)) -> T) {
+        injector.bind(builder: self) { i in
+            let params = (i.rp(P1.self), i.rp(P2.self), i.rp(P3.self), i.rp(P4.self), i.rp(P5.self), i.rp(P6.self))
+            return binding(params)
+        }
+    }
+
+    func with<P1, P2, P3, P4, P5, P6, P7>(_ binding: @escaping ((P1, P2, P3, P4, P5, P6, P7)) -> T) {
+        injector.bind(builder: self) { i in
+            let params = (i.rp(P1.self), i.rp(P2.self), i.rp(P3.self), i.rp(P4.self), i.rp(P5.self), i.rp(P6.self), i.rp(P7.self))
+            return binding(params)
+        }
+    }
+
+    func with<P1, P2, P3, P4, P5, P6, P7, P8>(_ binding: @escaping ((P1, P2, P3, P4, P5, P6, P7, P8)) -> T) {
+        injector.bind(builder: self) { i in
+            let params = (i.rp(P1.self), i.rp(P2.self), i.rp(P3.self), i.rp(P4.self), i.rp(P5.self), i.rp(P6.self), i.rp(P7.self), i.rp(P8.self))
+            return binding(params)
+        }
+    }
+
+    func with<P1, P2, P3, P4, P5, P6, P7, P8, P9>(_ binding: @escaping ((P1, P2, P3, P4, P5, P6, P7, P8, P9)) -> T) {
+        injector.bind(builder: self) { i in
+            let params = (i.rp(P1.self), i.rp(P2.self), i.rp(P3.self), i.rp(P4.self), i.rp(P5.self), i.rp(P6.self), i.rp(P7.self), i.rp(P8.self), i.rp(P9.self))
+            return binding(params)
+        }
+    }
+
+    func with<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10>(_ binding: @escaping ((P1, P2, P3, P4, P5, P6, P7, P8, P9, P10)) -> T) {
+        injector.bind(builder: self) { i in
+            let params = (i.rp(P1.self), i.rp(P2.self), i.rp(P3.self), i.rp(P4.self), i.rp(P5.self), i.rp(P6.self), i.rp(P7.self), i.rp(P8.self), i.rp(P9.self), i.rp(P10.self))
+            return binding(params)
+        }
+    }
+
+    func with<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11>(_ binding: @escaping ((P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11)) -> T) {
+        injector.bind(builder: self) { i in
+            let params = (i.rp(P1.self), i.rp(P2.self), i.rp(P3.self), i.rp(P4.self), i.rp(P5.self), i.rp(P6.self), i.rp(P7.self), i.rp(P8.self), i.rp(P9.self), i.rp(P10.self), i.rp(P11.self))
+            return binding(params)
+        }
+    }
+
+    func with<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12>(_ binding: @escaping ((P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12)) -> T) {
+        injector.bind(builder: self) { i in
+            let params = (i.rp(P1.self), i.rp(P2.self), i.rp(P3.self), i.rp(P4.self), i.rp(P5.self), i.rp(P6.self), i.rp(P7.self), i.rp(P8.self), i.rp(P9.self), i.rp(P10.self), i.rp(P11.self), i.rp(P12.self))
+            return binding(params)
+        }
+    }
+
+    func with<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13>(_ binding: @escaping ((P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13)) -> T) {
+        injector.bind(builder: self) { i in
+            let params = (i.rp(P1.self), i.rp(P2.self), i.rp(P3.self), i.rp(P4.self), i.rp(P5.self), i.rp(P6.self), i.rp(P7.self), i.rp(P8.self), i.rp(P9.self), i.rp(P10.self), i.rp(P11.self), i.rp(P12.self), i.rp(P13.self))
+            return binding(params)
+        }
+    }
 }

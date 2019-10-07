e31868ab6435605970880d45d68597239f706eff
Andrey Chernoprudov <dinloq@gmail.com>
2019-10-07 16:46:45 +0500

    Library header

---
+++ Sources/SwiftDI.h
@@ -0,0 +1,15 @@
+//
+//  SwiftDI.h
+//  SwiftDI
+//
+//  Created by Andrey Chernoprudov on 07/04/2018.
+//  Copyright © 2018 Little Stars. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for SwiftDI.
+FOUNDATION_EXPORT double SwiftDIVersionNumber;
+
+//! Project version string for SwiftDI.
+FOUNDATION_EXPORT const unsigned char SwiftDIVersionString[];
--- Sources/SwiftDI/SwiftDI.h
+++
@@ -1,8 +0,0 @@
-//
-//  SwiftDI.h
-//  SwiftDI
-//
-//  Created by Andrey Chernoprudov on 07/04/2018.
-//  Copyright © 2018 Little Stars. All rights reserved.
-//
-

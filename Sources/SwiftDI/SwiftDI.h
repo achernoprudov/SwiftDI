03858af1056b2bc25200d5c388bde780679d952e
Andrey Chernoprudov <dinloq@gmail.com>
2019-10-08 13:55:24 +0500

    modulemap

---
+++ Sources/SwiftDI/SwiftDI.h
@@ -0,0 +1,7 @@
+@import Foundation;
+
+//! Project version number for SwiftDI.
+FOUNDATION_EXPORT double SwiftDIVersionNumber;
+
+//! Project version string for SwiftDI.
+FOUNDATION_EXPORT const unsigned char SwiftDIVersionString[];
